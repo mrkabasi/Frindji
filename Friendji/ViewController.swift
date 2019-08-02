40e12a43e7d8b75cf99f6911f418ed6b8417bd02
Kasra Abasi <mr.k.abasi@gmail.com>
2019-08-02 17:07:51 +0430

    Initial Commit

---
+++ Friendji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Friendji
+//
+//  Created by Kasra Abasi on 8/2/19.
+//  Copyright © 2019 kasraabasi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
